c857daaa35e35bd090f1adcc6da78c675016c84f
vishnu <gvkvish@gmail.com>
2024-07-23 21:14:30 -0500

    Classes and Objects Project

---
+++ Clasess_objects/src/Employee.java
@@ -0,0 +1,24 @@
+
+public class Employee {
+
+		private int empNumber;
+		private String name;
+		
+		public int getEmpNumber() {
+			return empNumber;
+		}
+		
+		public void  setEmpNumber(int empNumber) {
+			this.empNumber = empNumber;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name= name;
+		}
+		
+		
+}
+
---
+++ Clasess_objects/src/MainApp.java
@@ -0,0 +1,18 @@
+
+public class MainApp {
+	
+	public static void main(String[] args) {
+		
+		Employee e = new Employee();
+		
+			e.setEmpNumber(101);
+			e.setName("vishnu");
+			
+			System.out.println(e.getEmpNumber());
+			System.out.println(e.getName());
+			
+			
+		
+	}
+
+}
